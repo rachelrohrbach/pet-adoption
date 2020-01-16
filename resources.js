a1e968cff3545f12037176d71a18054971ae7ad3
Rachel Rohrbach <rachel.rohrbach@gmail.com>
2020-01-16 08:16:27 -0800

    removed API key from google query

--- resources.js
+++ resources.js
@@ -1,9 +1,12 @@
-var apiKeyParam = "AIzaSyAZ4k61bFrYsE0QspDSD33TmODyZakds0U";
-var queryURL = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=mongolian%20grill&inputtype=textquery&fields=photos,formatted_address,name,opening_hours,rating&locationbias=circle:2000@47.6918452,-122.2226413&key=" + apiKeyParam;
+var apiKeyParam = "";
+var queryURL = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Museum%20of%20Contemporary%20Art%20Australia&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=" + apiKeyParam;
 
 $.ajax({
     url: queryURL,
     method: "GET",
+    headers: {
+    "Access-Control-Allow-Origin": "*"
+    }
 }).then(function(response) {
     console.log(response);
 })

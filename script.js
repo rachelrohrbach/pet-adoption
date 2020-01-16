22c2db712f1741edcc0bcc8ed058469e061f4501
Rachel Rohrbach <rachel.rohrbach@gmail.com>
2020-01-15 23:09:22 -0800

    added distance filter, removed breed and sex filters, and fixed ajax call

--- script.js
+++ script.js
@@ -9,127 +9,133 @@
 //   console.log(result);
 // });
 
-var apiKey = "?apikey=kCM2atMx";
+$(document).ready(function() {
+  $("#submit").on("click", function() {
+    // event.preventDefault();
 
-var queryURL1 =
-  // "https://api.rescuegroups.org/v5/public/animals/search?include=pictures,species&fields%5Banimals%5D=name,sex,breedPrimary,descriptionText,id,sizeGroup&fields%5Bpictures%5D=large&fields%5Bspecies%5D=singular&postalcode=98104";
-  "https://api.rescuegroups.org/v5/public/animals/search/available/dogs/haspic?include=breeds,colors,fosters,locations,orgs,patterns,pictures,species,videos,videourls";
+    // var species = $("#species").val();
 
-$.ajax({
-  url: queryURL1,
-  method: "POST",
-  // contentType: "application/vnd.api+json",
-  contentType: "application/json",
-  headers: {
-    Authorization: "kCM2atMx"
-  },
-  data: {
-    filters: [
-      // {
-      //   fieldName: "statuses.name",
-      //   operation: "equals",
-      //   criteria: "Available"
-      // }
-      //     // }
-      // {
-      //   fieldName: "species.singular",
-      //   operation: "equals",
-      //   criteria: "Cat"
-      // },
-      // {
-      //   fieldName: "species.singular",
-      //   operation: "equals",
-      //   criteria: "Dog"
-      // }
-    ],
-    // filterProcessing: "1 AND (2 OR 3)",
-    filterRadius: {
-      miles: 20,
-      postalcode: "98104"
-    }
-  }
+    // function isCat() {
+    //   if (species === "cat") {
+    //   }
+    // }
 
-  // filterRadius: {
-  //     miles: 10,
-  //     postalcode: "98104"
-  // }
-  // }
-  // data: {
-  //     filters:
-  //     [
-  //     ],
-  //       filterRadius: {
-  //         "miles": 20,
-  //         "postalcode": "98104" }
-  //   }
+    // var apiKey = "?apikey=kCM2atMx";
 
-  //  "data": {
-  //       "filters": [
-  //           {
-  //               "fieldName": "statuses.name",
-  //               "operation": "equals",
-  //               "criteria": "Available"
-  //           },
-  //           {
-  //               "fieldName": "species.singular",
-  //               "operation": "equals",
-  //               "criteria": ["Cat", "Dog"]
-  //           }
-  //       ],
-  //       "filterRadius": {
-  //           "miles": 50,
-  //           "postalcode": "98104"
-  //       }
-  //   }
+    var queryURL1 =
+      // "https://api.rescuegroups.org/v5/public/animals/search?include=pictures,species&fields%5Banimals%5D=name,sex,breedPrimary,descriptionText,id,sizeGroup&fields%5Bpictures%5D=large&fields%5Bspecies%5D=singular&postalcode=98104";
+      "https://api.rescuegroups.org/v5/public/animals/search/available/haspic?include=locations,orgs"; // ?include=breeds,colors,fosters,locations,orgs,patterns,pictures,species,videos,videourls";
 
-  // data: {
-  //   "filters": [
-  //       {
-  //             "fieldName": "species.name",
-  //             "operation": "equals",
-  //             "criteria": "Dog"
-  //         }
-  //     ]
-  // }
-}).then(function(result) {
-  console.log(result);
+    $.ajax({
+      url: queryURL1,
+      method: "POST",
+      contentType: "application/json",
+      headers: {
+        Authorization: "kCM2atMx"
+      },
+      data: JSON.stringify({
+        data: {
+          filters: [
+            {
+              fieldName: "statuses.name",
+              operation: "equals",
+              criteria: "Available"
+            },
+            {
+              fieldName: "species.singular",
+              operation: "equals",
+              criteria: $("#species option:selected").val()
+            }
+          ],
+          filterRadius: {
+            miles: $("#distance option:selected").val(),
+            postalcode: $("#zipCode")
+              .val()
+              .trim()
+          }
+        }
+      })
+    }).then(function(result) {
+      console.log(result);
 
-  $("#pet-results")
-    .html('<h4 class="mt-3">Search Results</h4>')
-    .append('<div class="row">');
+      var queryURL2 = "https://api.rescuegroups.org/v5/public/orgs/search/";
 
-  for (var i = 0; i < result.data.length - 10; i++) {
-    var col = $('<div>').addClass('column is-multiline').attr('id', 'petCard');
-    var title = $("<h4>")
-      .addClass("card-title")
-      .text(result.data[i].attributes.name);
-    var card = $("<div>").addClass("card");
-    var breed = $("<p>")
-      .addClass("card-text")
-      .text("Breed: " + result.data[i].attributes.breedPrimary);
-    var sex = $("<p>")
-      .addClass("card-text")
-      .text("Sex: " + result.data[i].attributes.sex);
-    var body = $("<div>").addClass("card-body");
-    var img = $("<img>").attr(
-      "src",
-      result.data[i].attributes.pictureThumbnailUrl
-    );
+      $.ajax({
+        url: queryURL2,
+        method: "POST",
+        contentType: "application/json",
+        headers: {
+          Authorization: "kCM2atMx"
+        },
+        data: JSON.stringify({
+          data: {
+            filters: [],
+            filterProcessing: "1",
+            filterRadius: {
+              miles: $("#distance option:selected").val(),
+              postalcode: $("#zipCode")
+                .val()
+                .trim()
+            }
+          }
+        })
+      }).then(function(response) {
+        var orgs = {};
+        for (var i = 0; i < response.data.length; i++) {
+          orgs[response.data[i].id] = {
+            name: response.data[i].attributes.name,
+            url: response.data[i].attributes.url
+          };
+        }
 
-    col.append(card.append(body.append(title, img, breed, sex)));
-    $("#pet-results .row").append(col);
-  }
+        $("#pet-results")
+          .html('<h4 class="mt-3">Search Results</h4>')
+          .append('<div class="row">');
+
+        for (var i = 0; i < result.data.length - 10; i++) {
+          var col = $("<div>")
+            .addClass("column is-4")
+            .attr("id", "petCard");
+          var title = $("<h4>")
+            .addClass("card-title")
+            .text(result.data[i].attributes.name);
+          var card = $("<div>").addClass("card");
+          var breed = $("<p>")
+            .addClass("card-text")
+            .text("Breed: " + result.data[i].attributes.breedString);
+          var sex = $("<p>")
+            .addClass("card-text")
+            .text("Sex: " + result.data[i].attributes.sex);
+          // var aboutPet = $("<p>")
+          //   .addClass("card-text")
+          //   .text(result.data[i].attributes.descriptionText);
+          var distance = $("<p>")
+            .addClass("card-text")
+            .text("Distance: " + result.data[i].attributes.distance + " miles");
+          var orgName = $("<p>")
+            .addClass("card-text")
+            .text(
+              "Organization: " +
+                orgs[result.data[i].relationships.orgs.data[0].id].name
+            );
+          var orgUrl = $("<a>")
+            .addClass("card-text")
+            .text(orgs[result.data[i].relationships.orgs.data[0].id].url)
+            .attr("href", orgs[result.data[i].relationships.orgs.data[0].id].url);
+          var body = $("<div>").addClass("card-body");
+          var img = $("<img>").attr(
+            "src",
+            result.data[i].attributes.pictureThumbnailUrl
+          );
+
+          col.append(
+            card.append(
+              body.append(title, img, breed, sex, distance, orgName, orgUrl)
+            )
+          );
+          $("#pet-results .row").append(col);
+        }
+      });
+    });
+  });
 });
-
-// var queryURL2 = "https://api.rescuegroups.org/v5/public/orgs/5951";
-
-// $.ajax({
-//   url: queryURL2,
-//   method: "Get",
-//   contentType: "application/vnd.api+json",
-//   headers: {
-//     Authorization: "kCM2atMx"
-//   }
-// }).then(function(result) {
-//   console.log(result);
-// });

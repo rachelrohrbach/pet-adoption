3e204dad73f6556467e33bba93fa62feaacf4e49
rachelrohrbach <rachel.rohrbach@gmail.com>
2020-01-16 08:25:55 -0800

    update resources.js
    removed google API key

--- resources.js
+++ resources.js
@@ -1,4 +1,4 @@
-var apiKeyParam = "AIzaSyAZ4k61bFrYsE0QspDSD33TmODyZakds0U";
+var apiKeyParam = ;
 var queryURL = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=mongolian%20grill&inputtype=textquery&fields=photos,formatted_address,name,opening_hours,rating&locationbias=circle:2000@47.6918452,-122.2226413&key=" + apiKeyParam;
 
 $.ajax({
